cbd67f68b40f227bf0ee40bdc55e1746aa3b56a2
pbrnx <pedrobarone2709@gmail.com>
2023-09-11 14:17:03 -0300

    Criação da estrutura do header

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,15 @@
 function Topo() {
     return (
-      <header>Teste</header>
+      <header>
+      <nav>            
+          <ul class="desktop-menu">           
+              <li><a href="../pages/main.html">Home</a></li>
+              <li><a href="../pages/contato.html">Contato</a></li>
+              <li><a href="../pages/login.html">Nosso Projeto</a></li>      
+              <li><a href="../index.html">Cadastre-se / Faça o login</a></li>          
+          </ul>
+      </nav>
+  </header>
     );
   }
   
