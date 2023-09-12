5c4487ef5ae418f7318dd8a1c8c8fe4d55f55700
pbrnx <pedrobarone2709@gmail.com>
2023-09-12 10:34:51 -0300

    Alterações no CSS

--- src/Landing.js
+++ src/Landing.js
@@ -9,9 +9,9 @@ function Landing() {
         <h1 className="titulo">Smart Grid</h1><br></br>
         <h2 className="titulo2">Construindo um futuro sustentável. <br></br> Somos a solução brilhante para a crise energética do Brasil.</h2><br></br>
         
-        {/* Div que envolve o botão */}
+        
         <div className="button-container">
-          <button className="saiba-mais-button">Saiba Mais</button>
+          <button className="saiba-mais-button">Saiba Mais ⚡</button>
         </div>
       </div>
       <img className="box" src={require('./images/index.png')} alt="Imagem Smart Grid" />
--- src/Rodape.js
+++ src/Rodape.js
@@ -6,7 +6,7 @@ function Rodape() {
     
     <footer className="footer-container">
       <div className="footer-content">
-        <p>&copy; 2023 Nome da Sua Empresa</p>
+        <p>&copy; 2023 Grupo 5 - 1ESPV - FIAP</p>
         <p>Todos os direitos reservados.</p>
       </div>
     </footer>
