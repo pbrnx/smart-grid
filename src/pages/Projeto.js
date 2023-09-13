ab5679d564ea548f2ec16df8113322be0c1fe173
pbrnx <pedrobarone2709@gmail.com>
2023-09-13 17:41:54 -0300

    inicio teste galeria

--- src/pages/Projeto.js
+++ src/pages/Projeto.js
@@ -6,8 +6,8 @@ function Projeto() {
         <>
         <h1 className='Teste'>TESTE</h1>
         <div className="gallery" align='center'>
-            <img className='imgGaleria' src={require('https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80')}/>
-            <img className='imgGaleria' src={require('https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80')}/>
+            <img className='imgGaleria' src={require('../images/skater.jpg')}/>
+            <img className='imgGaleria' src={require('../images/skater.jpg')}/>
             <img className='imgGaleria' src={require('../images/skater.jpg')}/>
             <img className='imgGaleria' src={require('../images/skater.jpg')}/>            
         </div>
