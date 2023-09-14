cbe0724e9675345cf07fb4988def7c603b2c3a92
pbrnx <pedrobarone2709@gmail.com>
2023-09-14 20:29:42 -0300

    Criação de cadastro, login e logout

--- src/pages/Cadastro.js
+++ src/pages/Cadastro.js
@@ -1,29 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Form.css';
 
-
 function Cadastro() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [checkboxChecked, setCheckboxChecked] = useState(false);
+
+  const handleNomeChange = (event) => {
+    setNome(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleSenhaChange = (event) => {
+    setSenha(event.target.value);
+  };
+
+  const handleCheckboxChange = (event) => {
+    setCheckboxChecked(event.target.checked);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const checkMail = email.trim();
+
+    // Validate the form inputs
+    if (nome.length === 0 || email.length === 0 || senha.length === 0 || !checkboxChecked || !checkMail.includes('@')) {
+      alert("Verifique se preencheu todos os campos corretamente!");
+      return; // Exit early if validation fails
+    }
+
+    // Retrieve existing users from localStorage or initialize an empty array
+    const existingUsers = JSON.parse(localStorage.getItem('users')) || [];
+
+    // Check if the email already exists in the list of users
+    const userExists = existingUsers.some((user) => user.email === email);
+
+    if (userExists) {
+      alert("Este email já está cadastrado!");
+      return; 
+    }
+
+    const newUser = {
+      nome,
+      email,
+      senha,
+    };
+
+
+    existingUsers.push(newUser);
+    localStorage.setItem('users', JSON.stringify(existingUsers));
+
+    setNome('');
+    setEmail('');
+    setSenha('');
+    setCheckboxChecked(false);
+
+    alert('Cadastro realizado com sucesso!');
+  };
+
   return (
     <>
-    <h1>Cadastre-se e faça parte do nosso projeto!</h1>
-    <form>
+      <h1>Cadastre-se e faça parte do nosso projeto!</h1>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="loginUsername">Nome:</label>
-        <input type="text" placeholder='Digite seu nome completo'required/>
-        <br/>
-    
+        <input type="text" placeholder='Digite seu nome completo' required value={nome} onChange={handleNomeChange} />
+        <br />
+
         <label htmlFor="loginPassword">Email:</label>
-        <input type="text" placeholder='Digite seu Email'required/>
-        <br/>
+        <input type="text" placeholder='Digite seu Email' required value={email} onChange={handleEmailChange} />
+        <br />
 
         <label htmlFor="loginPassword">Senha:</label>
-        <input type="password" id="loginPassword" placeholder="Digite sua senha" equired/>
-        <br/>
-    
-        <button type="button" id="loginButton">Cadastrar</button>
-    </form>
+        <input type="password" id="loginPassword" placeholder="Digite sua senha" required value={senha} onChange={handleSenhaChange} />
+        <br />
 
+        <input type="checkbox" id="checkbox" checked={checkboxChecked} onChange={handleCheckboxChange} />
+        <label htmlFor="checkbox">Aceito os termos e condições</label>
+        <br />
+
+        <button type="submit" id="loginButton">Cadastrar</button>
+      </form>
     </>
-
   );
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import '../styles/Form.css';
 
-
 function Login() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const history = useHistory();
+
+  const checkPreviousLogin = () => {
+    // Check if the user is already logged in using sessionStorage
+    const isLoggedIn = sessionStorage.getItem('isLoggedIn');
+
+    if (isLoggedIn) {
+      alert('Você já está logado, Usuário.');
+      // Redirect to another route (e.g., dashboard) if needed
+      history.push('/dashboard'); // Replace '/dashboard' with the actual route
+      return true; // Return true if already logged in
+    }
+
+    return false; // Return false if not logged in
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleSenhaChange = (event) => {
+    setSenha(event.target.value);
+  };
+
+  const handleLogin = () => {
+    const isAlreadyLoggedIn = checkPreviousLogin();
+
+    if (isAlreadyLoggedIn) {
+      return; // Do not proceed with login if already logged in
+    }
+
+    const cadastros = JSON.parse(localStorage.getItem('users')) || [];
+    const user = cadastros.find((cadastro) => cadastro.email === email.trim() && cadastro.senha === senha.trim());
+
+    if (user) {
+      // Set a sessionStorage flag to remember the user's login status
+      sessionStorage.setItem('isLoggedIn', 'true');
+
+      alert("Login bem sucedido! Bem-vindo " + user.nome + "!");
+      history.push('/dashboard'); // Redirect to another route after successful login
+    } else {
+      alert("Credenciais de Login não encontradas.");
+    }
+  };
+
+  const handleLogout = () => {
+    // Clear the sessionStorage flag to log the user out
+    sessionStorage.removeItem('isLoggedIn');
+    // Optionally, you can also clear other session data or perform other logout tasks
+    alert('Você foi desconectado.');
+  };
+
   return (
     <>
-    <h1>Faça Login!</h1>
-    <form>    
-        <label for="loginPassword">Email:</label>
-        <input type="text"   placeholder="Digite seu email" required/>
-        <br/>
+      <h1>Faça Login!</h1>
+      <form>
+        <label htmlFor="loginPassword">Email:</label>
+        <input type="text" placeholder="Digite seu email" required value={email} onChange={handleEmailChange} />
+        <br />
 
-        <label for="loginPassword">Senha:</label>
-        <input type="password" id="loginPassword" placeholder="Digite sua senha" required/>
-        <br/>
-    
-        <button type="button" id="loginButton">Cadastrar</button>
-    </form>
+        <label htmlFor="loginPassword">Senha:</label>
+        <input type="password" id="loginPassword" placeholder="Digite sua senha" required value={senha} onChange={handleSenhaChange} />
+        <br />
+        <div className='containerLoginButton'>
+          <button type="button" id='loginButton' onClick={handleLogin}>Login</button>
+          <button type="button" id='loginButton' onClick={handleLogout}>Logout</button>
+        </div>  
+      </form>
     </>
-
   );
 }
 
